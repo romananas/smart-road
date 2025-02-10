347a9ffe1a4a792f27f201f42b5082296892610f
romann <romann.belmer@gmail.com>
2025-02-10 15:11:11 +0100

    added some comments

--- src/assets.rs
+++ src/assets.rs
@@ -2,6 +2,8 @@ use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::image::LoadTexture;
 
+// ! optimisation probleme : texture is reloading from rom to ram each refresh.
+// TODO : putting the texture into a var that pass in the main.
 pub fn load_map(canvas: &mut Canvas<Window>) -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     
