3f5fbca83e44dd4c513e026d759d48fe8d9636c8
romann <romann.belmer@gmail.com>
2025-02-13 00:47:55 +0100

    Fixed bug : departure direction == arrival direction

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,6 @@
 use sdl2::EventPump;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use rand::prelude::*;
 use smart_road::cars::cars::Direction;
 
 use crate::cars;
@@ -13,18 +12,6 @@ pub enum Type {
     None, // nothing happen
 }
 
-fn gen_rand_direction() -> cars::Direction {
-    let mut rng = rand::rng();
-    match rng.random_range(0u8..3u8) {
-        0 => Direction::North,
-        1 => Direction::South,
-        2 => Direction::East,
-        3 => Direction::West,
-        _ => panic!("error in gen rand direction"),
-    }
-    
-}
-
 pub fn handle(ep: &mut EventPump) -> Type {
     for event in ep.poll_iter() {
         match event {
@@ -33,11 +20,14 @@ pub fn handle(ep: &mut EventPump) -> Type {
                 keycode: Some(Keycode::ESCAPE),
                 ..
             } => return Type::Quit,
-            Event::KeyDown { keycode: Some(Keycode::UP), .. } => return Type::SpawnCar(Direction::North,gen_rand_direction()),
-            Event::KeyDown { keycode: Some(Keycode::DOWN), .. } => return Type::SpawnCar(Direction::South,gen_rand_direction()),
-            Event::KeyDown { keycode: Some(Keycode::RIGHT), .. } => return Type::SpawnCar(Direction::East,gen_rand_direction()),
-            Event::KeyDown { keycode: Some(Keycode::LEFT), .. } =>return Type::SpawnCar(Direction::West,gen_rand_direction()),
-            Event::KeyDown { keycode: Some(Keycode::R), .. } =>return Type::SpawnCar(gen_rand_direction(),gen_rand_direction()),
+            Event::KeyDown { keycode: Some(Keycode::UP), .. } => return Type::SpawnCar(Direction::North,Direction::random_without(Direction::North)),
+            Event::KeyDown { keycode: Some(Keycode::DOWN), .. } => return Type::SpawnCar(Direction::South,Direction::random_without(Direction::South)),
+            Event::KeyDown { keycode: Some(Keycode::RIGHT), .. } => return Type::SpawnCar(Direction::East,Direction::random_without(Direction::East)),
+            Event::KeyDown { keycode: Some(Keycode::LEFT), .. } => return Type::SpawnCar(Direction::West,Direction::random_without(Direction::West)),
+            Event::KeyDown { keycode: Some(Keycode::R), .. } => {
+                let spawning_direction = Direction::random();
+                return Type::SpawnCar(spawning_direction,Direction::random_without(spawning_direction));
+            },
             _ => return Type::None,
         }
     };
